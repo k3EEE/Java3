290f27a73f676b9eb44824810d4861a7374fbd53
K3EEE <k_emam@hotmail.com>
2022-06-25 22:45:53 +0300

    second commit

--- src/com/github/four/myclass.java
+++ src/com/github/four/myclass.java
@@ -5,6 +5,9 @@ public class myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello 1");
+		
+		System.out.println("Hello 2");
+		
 	}
 
 }
